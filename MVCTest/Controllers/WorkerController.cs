21640ee72c947e08c573c0f1466e2f685fad9363
Yaroslav Prishedko <yaroslav05042005@gmail.com>
2021-05-30 23:25:05 +0300

    Добавьте файлы проекта.

---
+++ MVCTest/Controllers/WorkerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.Data;
+using MVCTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTest.Controllers
+{
+    public class WorkerController : Controller
+    {
+        public string Index()
+        {
+            Worker worker = Repository.GetWorker();
+
+            string output = $"The worker {worker.Name} is {worker.Age} y.o. and has a salary of {worker.Salary}$.";
+
+            return output;
+        }
+    }
+}
---
+++ MVCTest/Data/Repository.cs
@@ -0,0 +1,32 @@
+﻿using MVCTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTest.Data
+{
+    public class Repository
+    {
+        private static List<string> names;
+        private static Random r;
+        static Repository()
+        {
+            names = new List<string>() { "Mike", "Michelle", "Alina", "Bob" };
+            r = new Random();
+        }
+
+        public static Worker GetWorker()
+        {
+            string name = names[r.Next(names.Count)];
+            int salary = r.Next(8000,15000);
+            int age = r.Next(18, 90);
+            return new Worker()
+            {
+                Age = age,
+                Salary = salary,
+                Name = name
+            };
+        }
+    }
+}
